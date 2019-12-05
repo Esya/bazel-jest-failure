998898333c7ffc0a3d7b66dfc78d30c7157fce2f
Tristan Foureur <tristan.foureur@gmail.com>
2019-12-05 19:51:38 +0100

    adding a working run rule

---
+++ libs/add/src/manual.ts
@@ -0,0 +1,7 @@
+/**
+ * Making sure that normal resolution works.
+ */
+import { add } from './add'
+
+console.log("Checking if imports work : ", add(2,2));
+console.log(`Ran at ${__filename}`)
---
+++ libs/subtract/src/manual.ts
@@ -0,0 +1,7 @@
+/**
+ * Making sure that normal resolution works.
+ */
+import { subtract } from './subtract'
+
+console.log("Checking if imports work : ", subtract(4,2));
+console.log(`Ran at ${__filename}`)
